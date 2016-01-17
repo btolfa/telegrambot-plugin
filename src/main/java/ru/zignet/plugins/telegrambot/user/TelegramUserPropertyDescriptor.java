32ede38b6aa87045dab97a51a3a6144476b1b4e8
Tengiz Sharafiev <btolfa@gmail.com>
2016-01-18 00:35:19 +0300

    Using DataBoundConstructor

--- src/main/java/ru/zignet/plugins/telegrambot/user/TelegramUserPropertyDescriptor.java
+++ src/main/java/ru/zignet/plugins/telegrambot/user/TelegramUserPropertyDescriptor.java
@@ -5,22 +5,32 @@ package ru.zignet.plugins.telegrambot.user;
  * @date 2016-01-17
  */
 
+import hudson.Extension;
 import hudson.model.User;
 import hudson.model.UserProperty;
 import hudson.model.UserPropertyDescriptor;
 
+import hudson.util.FormValidation;
 import net.sf.json.JSONObject;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
 
 /**
  * Descriptor for Telegram user property.
  */
+@Extension
 public class TelegramUserPropertyDescriptor extends UserPropertyDescriptor {
-
-    public static final String PARAMETERNAME_USERNAME = "telegram.user.username";
 
     public TelegramUserPropertyDescriptor() {
         super(TelegramUserProperty.class);
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Telegram Username";
     }
 
     @Override
@@ -30,15 +40,20 @@ public class TelegramUserPropertyDescriptor extends UserPropertyDescriptor {
 
     @Override
     public UserProperty newInstance(StaplerRequest req, JSONObject formData) throws FormException {
-        try {
-            return new TelegramUserProperty(req.getParameter(PARAMETERNAME_USERNAME));
-        } catch (IllegalArgumentException e) {
-            throw new FormException("invalid Telegram Username", PARAMETERNAME_USERNAME);
-        }
+        return req.bindJSON(TelegramUserProperty.class, formData);
     }
 
-    @Override
-    public String getDisplayName() {
-        return "Telegram Username";
+    public FormValidation doCheckUsername(@QueryParameter String value) throws IOException, ServletException {
+        if (value.length() == 0) {
+            return FormValidation.error("Please set a username");
+        }
+        if (value.length() < 5) {
+            return FormValidation.error("The username must be at least five characters long");
+        }
+        if (! value.matches("[a-zA-Z0-9_]+")) {
+            return FormValidation.error("You must use a-z, 0-9 and underscores");
+        }
+
+        return FormValidation.ok();
     }
 }
